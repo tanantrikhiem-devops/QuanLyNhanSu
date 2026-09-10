4dbc33ac2955e730306a239b1857a663fcb360d5
torikkyun <nduc42176@gmail.com>
2026-09-10 13:30:29 +0700

    Thêm CORS

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.router import api_router
 from app.db.session import create_db_and_tables
@@ -18,3 +19,10 @@ app = FastAPI(lifespan=lifespan)
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.include_router(api_router, prefix="/api/v1")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
